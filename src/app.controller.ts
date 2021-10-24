a750c864c1017b4cf65a35ecb7a1894fd97e8efa
Iain <iain@zora.co>
2021-10-24 17:53:09 -0400

    update site info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -71,6 +71,7 @@ export class AppController {
 <html>
 <head>
 <title>ether.actor</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
 <style>
 body {
   font-family: helvetica;
@@ -85,6 +86,7 @@ li {
 </head>
 <body>
 <h2>super simple ethereum -> http bridge for fetching contract info</h2>
+<a class="github-fork-ribbon" href="https://github.com/iainnash/ether.actor" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
 <p>powered by etherscan for ABIs and cloudflare for ethereum endpoints<p>
 <br />
 <p>try it:</p>
@@ -103,6 +105,13 @@ returns the result of the given function call (works for media too)</li>
 <li><a href="/0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63/isApprovedForAll/0x18C8dF1fb7FB44549F90d1C2BB1DC8b690CD0559/0xd2d5cb7545685019a85601a0d279b24df019ec5e">/0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63/isApprovedForAll/0xADDRESS0/0xADDRESS1</a>
 <br />works with booleans and multiple arguments.
 </ul>
+
+<div style="margin-top: 20vh">
+  <a href="https://twitter.com/isiain">a project by iain</a>
+  <br />
+  <br />
+  <a href="https://github.com/iainnash/ether.actor">source code</a>
+</div>
 
 </body>
 </html>
