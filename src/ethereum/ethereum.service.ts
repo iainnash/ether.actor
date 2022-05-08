4c08cb95489bcd3dcd05cd6a54ad1bf557d2ce00
Iain <iain@zora.co>
2022-05-08 14:08:03 -0400

    service fixes

--- src/ethereum/ethereum.service.ts
+++ src/ethereum/ethereum.service.ts
@@ -3,11 +3,13 @@ import {
   BINANCE_NETWORK,
   BINANCE_TESTNET_NETWORK,
   EtherscanProvider,
+  GOERLI_NETWORK,
   InjectEthersProvider,
   MAINNET_NETWORK,
   MATIC_NETWORK,
   MUMBAI_NETWORK,
   RINKEBY_NETWORK,
+  ROPSTEN_NETWORK,
   StaticJsonRpcProvider,
 } from 'nestjs-ethers';
 
@@ -25,6 +27,21 @@ EtherscanProvider.prototype.getBaseUrl = function () {
   if (this.network.chainId === RINKEBY_NETWORK.chainId) {
     return 'https://api-rinkeby.etherscan.io/';
   }
+  if (this.network.chainId === BINANCE_NETWORK.chainId) {
+    return 'https://api.bscscan.com/';
+  }
+  if (this.network.chainId === BINANCE_TESTNET_NETWORK.chainId) {
+    return 'https://api-testnet.bscscan.com/';
+  }
+  // optimism
+  if (this.network.chainId === 10) {
+    return 'https://api-optimistic.etherscan.io/';
+  }
+  // kovan-optimism
+  if (this.network.chainId === 69) {
+    return 'https://api-kovan-optimistic.etherscan.io/';
+  }
+
   throw new Error('undefined chain');
 };
 
@@ -39,6 +56,18 @@ export class EthereumService {
     private readonly mumbaiProvider: StaticJsonRpcProvider,
     @InjectEthersProvider('polygon')
     private readonly polygonProvider: StaticJsonRpcProvider,
+    @InjectEthersProvider('bsc')
+    private readonly bscProvider: StaticJsonRpcProvider,
+    @InjectEthersProvider('bsc-testnet')
+    private readonly bscTestnetProvider: StaticJsonRpcProvider,
+    @InjectEthersProvider('optimism')
+    private readonly optimismProvider: StaticJsonRpcProvider,
+    @InjectEthersProvider('kovan-optimism')
+    private readonly kovanOptimismProvider: StaticJsonRpcProvider,
+    @InjectEthersProvider('ropsten')
+    private readonly ropstenProvider: StaticJsonRpcProvider,
+    @InjectEthersProvider('goerli')
+    private readonly goreliProvider: StaticJsonRpcProvider,
   ) {}
 
   getNetworkId(host: string) {
@@ -51,10 +80,18 @@ export class EthereumService {
         return MAINNET_NETWORK.chainId;
       case 'rinkeby':
         return RINKEBY_NETWORK.chainId;
+      case 'optimism':
+        return 10;
+      case 'kovan-optimism':
+        return 69;
       case 'mumbai':
         return MUMBAI_NETWORK.chainId;
       case 'polygon':
         return MATIC_NETWORK.chainId;
+      case 'goerli':
+        return GOERLI_NETWORK.chainId;
+      case 'ropsten':
+        return ROPSTEN_NETWORK.chainId;
       case 'bsc':
         return BINANCE_NETWORK.chainId;
       case 'bsc-testnet':
@@ -73,6 +110,18 @@ export class EthereumService {
         return this.mumbaiProvider;
       case MATIC_NETWORK.chainId:
         return this.polygonProvider;
+      case BINANCE_NETWORK.chainId:
+        return this.bscProvider;
+      case BINANCE_TESTNET_NETWORK.chainId:
+        return this.bscTestnetProvider;
+      case GOERLI_NETWORK.chainId:
+        return this.goreliProvider;
+      case ROPSTEN_NETWORK.chainId:
+        return this.ropstenProvider;
+      case 69:
+        return this.kovanOptimismProvider;
+      case 10:
+        return this.optimismProvider;
     }
   }
 
