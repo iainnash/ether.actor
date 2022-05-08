cb04be3ace016932da7ab5cbf1c17eec4a606283
Iain Nash <iain@zora.co>
2022-05-08 11:34:31 -0400

    add goreli, optimism, and ropsten (#20)
    * add goreli, optimism, and ropsten
    
    * fix networks config

--- src/ethereum/ethereum.module.ts
+++ src/ethereum/ethereum.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import {
   EthersModule,
+  GOERLI_NETWORK,
   MAINNET_NETWORK,
   MATIC_NETWORK,
   MUMBAI_NETWORK,
   RINKEBY_NETWORK,
+  ROPSTEN_NETWORK,
 } from 'nestjs-ethers';
 import { EthereumService } from './ethereum.service';
 
@@ -36,6 +38,35 @@ const NETWORK_CONFIGS = JSON.parse(process.env.RPC_NETWORK_CONFIGS);
       token: 'polygon',
       network: MATIC_NETWORK.chainId,
       custom: NETWORK_CONFIGS.polygon,
+      useDefaultProvider: false,
+    }),
+    EthersModule.forRoot({
+      token: 'goerli',
+      network: GOERLI_NETWORK,
+      custom: NETWORK_CONFIGS.goerli || 'https://goerli.prylabs.net',
+      useDefaultProvider: false,
+    }),
+    EthersModule.forRoot({
+      token: 'ropsten',
+      network: ROPSTEN_NETWORK,
+      custom: NETWORK_CONFIGS.ropsten,
+    }),
+    EthersModule.forRoot({
+      token: 'kovan-optimism',
+      network: {
+        chainId: 69,
+        name: 'kovan-optimism',
+      },
+      custom: NETWORK_CONFIGS.kovan_optimism || 'https://kovan.optimism.io',
+      useDefaultProvider: false,
+    }),
+    EthersModule.forRoot({
+      token: 'optimism',
+      network: {
+        chainId: 10,
+        name: 'optimism',
+      },
+      custom: NETWORK_CONFIGS.optimism || 'https://mainnet.optimism.io',
       useDefaultProvider: false,
     }),
   ],
